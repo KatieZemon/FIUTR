c627cdbe050efc843b4419c4986407010196dea9
Katie <kjihdc@mst.edu>
2013-10-31 19:51:27 -0500

    Added Values to display the progress of each SeekBar

--- client/src/com/example/fiutr/SearchActivity.java
+++ client/src/com/example/fiutr/SearchActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Build;
 import android.os.Bundle;
+import android.support.v4.app.NavUtils;
 import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
@@ -15,44 +16,64 @@ import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.SeekBar;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class SearchActivity extends Activity {
 
-	// private EditText prefEditText;
-	private SeekBar prefDistSeekbar, prefSignalSeekbar, prefResultsSeekbar;
-
+	// SeekBars for setting preferences
+	private SeekBar prefDistSeekbar;
+	private SeekBar prefSignalSeekbar;
+	private SeekBar prefResultsSeekbar;
+	
+	// Values of our preferences
+	private TextView prefDistVal;
+	private TextView prefSignalVal;
+	private TextView prefResultsVal;
+	
+	// Search button
+	private Button searchButton;
+	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_search);
 
-		SharedPreferences customSharedPreference = getSharedPreferences(
-				"myCustomSharedPrefs", Activity.MODE_PRIVATE);
+		SharedPreferences customSharedPreference = getSharedPreferences("myCustomSharedPrefs", Activity.MODE_PRIVATE);
 
+		// Distance preference
 		prefDistSeekbar = (SeekBar) findViewById(R.id.seek_distance);
-		prefDistSeekbar.setProgress(customSharedPreference.getInt("myDistPref",
-				50));
+		prefDistSeekbar.setProgress(customSharedPreference.getInt("myDistPref",50));
+		prefDistSeekbar.setOnSeekBarChangeListener(new SeekBarListener());
+		
+		prefDistVal = (TextView) findViewById(R.id.text_distVal);
+		prefDistVal.setText(""+customSharedPreference.getInt("myDistPref",50)+" miles");
 
+		// Signal Strength Preference
 		prefSignalSeekbar = (SeekBar) findViewById(R.id.seek_signal);
-		prefSignalSeekbar.setProgress(customSharedPreference.getInt(
-				"mySignalPref", 50));
+		prefSignalSeekbar.setProgress(customSharedPreference.getInt("mySignalPref", 50));
+		prefSignalSeekbar.setOnSeekBarChangeListener(new SeekBarListener());
+		
+		prefSignalVal = (TextView) findViewById(R.id.text_signalVal);
+		prefSignalVal.setText(""+customSharedPreference.getInt("mySignalPref",50));
 
+		// Results Preference
 		prefResultsSeekbar = (SeekBar) findViewById(R.id.seek_results);
-		prefResultsSeekbar.setProgress(customSharedPreference.getInt(
-				"myResultsPref", 50));
-		/*
-		 * Button mClose = (Button) findViewById(R.id.close);
-		 * mClose.setOnClickListener(new OnClickListener() { public void
-		 * onClick(View v) { finish(); } });
-		 */
-
-		Button mSave = (Button) findViewById(R.id.save);
-		mSave.setOnClickListener(new OnClickListener() {
+		prefResultsSeekbar.setProgress(customSharedPreference.getInt("myResultsPref", 50));
+		prefResultsSeekbar.setOnSeekBarChangeListener(new SeekBarListener());
+		
+		prefResultsVal = (TextView) findViewById(R.id.text_resultsVal);
+		prefResultsVal.setText(""+customSharedPreference.getInt("myResultsPref",50));
+		
+		// Search Button
+		searchButton = (Button) findViewById(R.id.save);
+		searchButton.setOnClickListener(new OnClickListener() {
 			public void onClick(View v) {
 				savePreferences();
 				finish();
 			}
 		});
+		
 
 		// Make sure we're running on Honeycomb or higher to use ActionBar APIs
 		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
@@ -60,14 +81,33 @@ public class SearchActivity extends Activity {
 			getActionBar().setDisplayHomeAsUpEnabled(true);
 		}
 	}
+	
+	private class SeekBarListener implements SeekBar.OnSeekBarChangeListener {
+
+        public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+            if (seekBar == prefDistSeekbar)
+            	prefDistVal.setText(""+progress+ " miles");
+            else if (seekBar == prefSignalSeekbar)
+            	prefSignalVal.setText(""+progress);
+            else if (seekBar == prefResultsSeekbar)
+            	prefResultsVal.setText(""+progress);
+        }
+
+        public void onStartTrackingTouch(SeekBar seekBar) {}
+
+        public void onStopTrackingTouch(SeekBar seekBar) {}
+
+    }
+ 
 
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
 		switch (item.getItemId()) {
 		case android.R.id.home:
-			// NavUtils.navigateUpFromSameTask(this);
-			Intent intent = new Intent(this, MainActivity.class);
-			startActivity(intent);
+			//NavUtils.navigateUpFromSameTask(this);
+			finish();
+			//Intent intent = new Intent(this, MainActivity.class);
+			//startActivity(intent);
 			return true;
 		}
 
