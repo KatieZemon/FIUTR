5b60b99dd33150c4cc35fc8a1dc4914655472b98
Rschmitt13 <r.schmitt08@gmail.com>
2013-10-31 21:48:33 -0500

    Meow

--- client/src/com/example/fiutr/ScanActivity.java
+++ client/src/com/example/fiutr/ScanActivity.java
@@ -1,34 +1,47 @@
 package com.example.fiutr;
-
-import android.content.Intent;
-import android.os.Build;
+import android.widget.CheckBox;
+import android.widget.ScrollView;
+import java.util.ArrayList;
 import android.os.Bundle;
-import android.preference.PreferenceActivity;
-import android.view.MenuItem;
+import android.app.Activity;
+import android.view.Menu;
+import android.view.View;
 
-public class ScanActivity extends PreferenceActivity {
+public class ScanActivity extends Activity {
+    ArrayList<CheckBox> SSIDS; //an ArrayList of type CheckBox, currently hand-populated, will eventually be populated from the list of SSIDs available
+    ScrollView SSID_View = (ScrollView)findViewById(R.id.SSIDV); //variable to reference the ScrollView
+    CheckBox C_Scan = (CheckBox)findViewById(R.id.checkBox1); //variable to reference the checkbox for continuous scanning
+    
 	@Override
-	public void onCreate(Bundle savedInstanceState) {
+	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		addPreferencesFromResource(R.layout.activity_scan);
-
-		// Make sure we're running on Honeycomb or higher to use ActionBar APIs
-		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
-			// Show the Up button in the action bar.
-			getActionBar().setDisplayHomeAsUpEnabled(true);
-		}
+		setContentView(R.layout.activity_scan);
+		
+		for(int i = 0; i < 10; i++){ //add the SSIDS to the ListView
+			SSIDS.add(new CheckBox(this));
+			SSIDS.get(i).setText("SSID "+i);
+			SSID_View.addView(SSIDS.get(i));
+			}
 	}
 
 	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		switch (item.getItemId()) {
-		case android.R.id.home:
-			// NavUtils.navigateUpFromSameTask(this);
-			Intent intent = new Intent(this, MainActivity.class);
-			startActivity(intent);
-			return true;
-		}
-
-		return super.onOptionsItemSelected(item);
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.scan, menu);
+		return true;
 	}
+	
+	public void OnClick(View v){ //when the continuous scan checkbox is checked, the SSID's personal checkboxes are disabled
+		if (C_Scan.isChecked()){
+			for (CheckBox cb : SSIDS){
+				cb.setEnabled(false);
+			}
+		}
+		else{ //if unchecked reenabled
+			for (CheckBox cb : SSIDS){
+				cb.setEnabled(true);
+			}
+		}
+	}
+
 }
