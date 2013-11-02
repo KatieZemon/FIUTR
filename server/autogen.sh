0d93cb8f8488de6b7cac5e6fb25ddf340020015a
Michael Catanzaro <michael.catanzaro@mst.edu>
2013-11-01 19:51:19 -0500

    Add a sanity check to autogen.sh

--- server/autogen.sh
+++ server/autogen.sh
@@ -1,5 +1,14 @@
 #!/bin/sh -e
 
+# standard sanity-check for GNOME projects
+srcdir=`dirname $0`
+test -z "$srcdir" && srcdir=.
+(test -f $srcdir/src/connection_manager.cc) || {
+    echo -n "**Error**: Directory "\`$srcdir\'" does not look like the"
+    echo " top-level groupgd directory"
+    exit 1
+}
+
 # Taken from systemd autogen.sh. No committing whitespace errors.
 if [ -f .git/hooks/pre-commit.sample ] && [ ! -f .git/hooks/pre-commit ]; then
         # This part is allowed to fail
